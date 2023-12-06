d60d1df51e292ae162bdc0df6d296cba3c68d354
Patryk Hegenberg <hegenberg@tixeltec.com>
2023-12-06 08:38:23 +0100

    add dark-mode and theme switcher

--- main.go
+++ main.go
@@ -14,12 +14,15 @@ var (
 	chars []model.Character
 	//go:embed templates/*.html
 	//go:embed images/*
-	content  embed.FS
+	content embed.FS
+	//go:embed static/*
+	static   embed.FS
 	Monsters []model.Monster
 )
 
 func main() {
 	filename := ""
+	log.Printf("Eingebunden is %v\n", static)
 
 	http.HandleFunc("/", handlers.FormHandler(content, &Monsters, filename))
 	http.HandleFunc("/submit", handlers.SubmitHandler(content, &chars, &Monsters, filename))
@@ -29,6 +32,27 @@ func main() {
 	http.HandleFunc("/contact", handlers.ContactHandler(content))
 	http.HandleFunc("/monsterTable", handlers.MonsterTableHandler(content, &Monsters))
 
+	// Lade die CSS-Datei
+	css, err := loadCSS(static)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Füge eine Route für die CSS-Datei hinzu
+	http.HandleFunc("/static/darkly_bulmawatch.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		w.Write([]byte(css))
+	})
+
 	log.Print("Server gestartet, erreichbar unter http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+// loadCSS liest die CSS-Datei aus dem eingebetteten Dateisystem.
+func loadCSS(content embed.FS) (string, error) {
+	file, err := content.ReadFile("static/darkly_bulmawatch.css")
+	if err != nil {
+		return "", err
+	}
+	return string(file), nil
 }
