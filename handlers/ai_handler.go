c8553ae5e43abae8e109745be0a4c6bfcf159969
krzysztof <stona013@hotmail.de>
2024-06-10 22:57:35 +0200

    fixed txt file to be a readable txt file instead of the json format

--- handlers/ai_handler.go
+++ handlers/ai_handler.go
@@ -44,38 +44,72 @@ func AIHandler(content embed.FS, monsters *[]model.Monster) http.HandlerFunc {
 			fileFormat := r.FormValue("fileFormat") // Get the file format from the form
 
 			var prompt string
-			if generationType == "random" {
-				if extra == "innate_SP" {
-					prompt = `Create a detailed and unique Homebrew DnD monster with the following Json structure but input the stats and features from the monster you have built:
+
+			if fileFormat == "json" {
+				if generationType == "random" {
+					if extra == "innate_SP" {
+						prompt = `Create a detailed and unique Homebrew DnD monster with the following Json structure but input the stats and features from the monster you have built:
 		json structure:
 		{ "_meta":{"_dateLastModifiedHash":"66658f76","sources":[{"json":"chatbrewery","abbreviation":"MG","version":"unknown","authors":["Krzysztof"],"convertedBy":["Krzysztof"]}],"dateAdded":1717931894,"dateLastModified":1717931894},"monster":[{"save":{"dex":"1","con":"1","wis":"1","cha":"1","str":"1","int":"1"},"skill":{"stealth":"-3","acrobatics":"-3","animalHandling":"-3","arcana":"-3","athletics":"-3","deception":"-3","history":"-3","insight":"-3","intimidation":"-3","investigation":"-3","medicine":"-3","nature":"-3","perception":"-3","performance":"-3","persuasion":"-3","sleightOfHand":"-3","survival":"-3","religion":"-3"},"hp":{"formula":"1","average":1},"source":"1","cr":"1","type":"beast","name":"test","vulnerable":["1"],"conditionImmune":["1"],"resist":["1"],"immune":["1"],"trait":[{"name":"1","entries":["1"]}],"ac":[{"from":["1"],"ac":1}],"senses":["1"],"languages":["1"],"size":["H"],"action":[{"name":"Claw","entries":["{@atk mw} {@hit 7} to hit, reach 5 ft., one target. {@h}17 ({@damage 2d12 + 5}) bludgeoning damage. On a hit, the target must make a DC 16 Strength saving throw or be knocked prone."]}],"speed":{"walk":1,"burrow":1,"climb":1,"fly":1,"swim":1},"spellcasting": [{"name": "Innate Spellcasting","headerEntries": ["The monster innate spellcasting ability is Wisdom (spell save {@dc 16}, +8 to hit with spell attacks). It can innately cast the following spells, requiring no components:"],"will": ["{@spell spellname}"],"daily": {"3e": ["{@spell spellname}","{@spell spellname}",],"1e": ["{@spell spellname}","{@spell spellname}",]},"ability": "wis","type": "spellcasting"}],"spellcastingTags": ["P","I"],"str":1,"dex":1,"con":1,"int":1,"wis":1,"cha":1}]}
 		make sure the monster name, type, are different each time and for the CR choose a Number between 1 and 30 and don’t use the word Gloom as a Monster name and respond with the Json structure and nothing else. Do not put a backtick at the beginning and the end!`
-				} else {
-					prompt = `Create a detailed and unique Homebrew DnD monster with the following Json structure but input the stats and features from the monster you have built:
+					} else {
+						prompt = `Create a detailed and unique Homebrew DnD monster with the following Json structure but input the stats and features from the monster you have built:
 		json structure:
 		{ "_meta":{"_dateLastModifiedHash":"66658f76","sources":[{"json":"Malgorgon","abbreviation":"MG","version":"unknown","authors":["Krzysztof"],"convertedBy":["Krzysztof"]}],"dateAdded":1717931894,"dateLastModified":1717931894},"monster":[{"save":{"dex":"1","con":"1","wis":"1","cha":"1","str":"1","int":"1"},"skill":{"stealth":"-3","acrobatics":"-3","animalHandling":"-3","arcana":"-3","athletics":"-3","deception":"-3","history":"-3","insight":"-3","intimidation":"-3","investigation":"-3","medicine":"-3","nature":"-3","perception":"-3","performance":"-3","persuasion":"-3","sleightOfHand":"-3","survival":"-3","religion":"-3"},"hp":{"formula":"1","average":1},"source":"1","cr":"1","type":"beast","name":"test","vulnerable":["1"],"conditionImmune":["1"],"resist":["1"],"immune":["1"],"trait":[{"name":"1","entries":["1"]}],"ac":[{"from":["1"],"ac":1}],"senses":["1"],"languages":["1"],"size":["H"],"action":[{"name":"Claw","entries":["{@atk mw} {@hit 7} to hit, reach 5 ft., one target. {@h}17 ({@damage 2d12 + 5}) bludgeoning damage. On a hit, the target must make a DC 16 Strength saving throw or be knocked prone."]}],"speed":{"walk":1,"burrow":1,"climb":1,"fly":1,"swim":1},"str":1,"dex":1,"con":1,"int":1,"wis":1,"cha":1}]}
 		make sure the monster name, type, are different each time and for the CR choose a Number between 1 and 30 and don’t use the word Gloom as a Monster name and respond with the Json structure and nothing else. Do not put a backtick at the beginning and the end!`
+					}
+				} else {
+					name := r.FormValue("name")
+					cr := r.FormValue("cr")
+					monsterType := r.FormValue("type")
+					monsterInfo := r.FormValue("monsterinfo")
+					prompt = `Create a detailed DnD monster for the plutonium importer tool from 5etools for the import on Foundry Vtt with the following Json structure just respond with the Json structure and nothing else not even a command from you and also don’t put backtick at the beginning and the end!!:
+		json structure:
+		{ "_meta":{"_dateLastModifiedHash":"66658f76","sources":[{"json":"chatbrewery","abbreviation":"MG","version":"unknown","authors":["Krzysztof"],"convertedBy":["Krzysztof"]}],"dateAdded":1717931894,"dateLastModified":1717931894},"monster":[{"save":{"dex":"1","con":"1","wis":"1","cha":"1","str":"1","int":"1"},"skill":{"stealth":"-3","acrobatics":"-3","animalHandling":"-3","arcana":"-3","athletics":"-3","deception":"-3","history":"-3","insight":"-3","intimidation":"-3","investigation":"-3","medicine":"-3","nature":"-3","perception":"-3","performance":"-3","persuasion":"-3","sleightOfHand":"-3","survival":"-3","religion":"-3"},"hp":{"formula":"1","average":1},"source":"1","cr":"1","type":"beast","name":"test","vulnerable":["1"],"conditionImmune":["1"],"resist":["1"],"immune":["1"],"trait":[{"name":"1","entries":["1"]}],"ac":[{"from":["1"],"ac":1}],"senses":["1"],"languages":["1"],"size":["H"],"action":[{"name":"Claw","entries":["{@atk mw} {@hit 7} to hit, reach 5 ft., one target. {@h}17 ({@damage 2d12 + 5}) bludgeoning damage. On a hit, the target must make a DC 16 Strength saving throw or be knocked prone."]}],"speed":{"walk":1,"burrow":1,"climb":1,"fly":1,"swim":1},"str":1,"dex":1,"con":1,"int":1,"wis":1,"cha":1}]}`
+
+					if name != "" {
+						prompt += "\nName: " + name
+					}
+					if cr != "" {
+						prompt += "\nCR: " + cr
+					}
+					if monsterType != "" {
+						prompt += "\nType: " + monsterType
+					}
+					if monsterInfo != "" {
+						prompt += "\nDetails: " + monsterInfo
+					}
 				}
 			} else {
-				name := r.FormValue("name")
-				cr := r.FormValue("cr")
-				monsterType := r.FormValue("type")
-				monsterInfo := r.FormValue("monsterinfo")
-				prompt = `Create a detailed DnD monster for the plutonium importer tool from 5etools for the import on Foundry Vtt with the following Json structure just respond with the Json structure and nothing else not even a command from you and also don’t put backtick at the beginning and the end!!:
-		json structure:
-		{ "_meta":{"_dateLastModifiedHash":"66658f76","sources":[{"json":"Malgorgon","abbreviation":"MG","version":"unknown","authors":["Krzysztof"],"convertedBy":["Krzysztof"]}],"dateAdded":1717931894,"dateLastModified":1717931894},"monster":[{"save":{"dex":"1","con":"1","wis":"1","cha":"1","str":"1","int":"1"},"skill":{"stealth":"-3","acrobatics":"-3","animalHandling":"-3","arcana":"-3","athletics":"-3","deception":"-3","history":"-3","insight":"-3","intimidation":"-3","investigation":"-3","medicine":"-3","nature":"-3","perception":"-3","performance":"-3","persuasion":"-3","sleightOfHand":"-3","survival":"-3","religion":"-3"},"hp":{"formula":"1","average":1},"source":"1","cr":"1","type":"beast","name":"test","vulnerable":["1"],"conditionImmune":["1"],"resist":["1"],"immune":["1"],"trait":[{"name":"1","entries":["1"]}],"ac":[{"from":["1"],"ac":1}],"senses":["1"],"languages":["1"],"size":["H"],"action":[{"name":"Claw","entries":["{@atk mw} {@hit 7} to hit, reach 5 ft., one target. {@h}17 ({@damage 2d12 + 5}) bludgeoning damage. On a hit, the target must make a DC 16 Strength saving throw or be knocked prone."]}],"speed":{"walk":1,"burrow":1,"climb":1,"fly":1,"swim":1},"str":1,"dex":1,"con":1,"int":1,"wis":1,"cha":1}]}`
 
-				if name != "" {
-					prompt += "\nName: " + name
-				}
-				if cr != "" {
-					prompt += "\nCR: " + cr
-				}
-				if monsterType != "" {
-					prompt += "\nType: " + monsterType
-				}
-				if monsterInfo != "" {
-					prompt += "\nDetails: " + monsterInfo
+				if generationType == "random" {
+					if extra == "innate_SP" {
+						prompt = `Create a detailed and unique Homebrew DnD monster with innate Spellcasting you choose the monster name, type, are different 
+					each time and for the CR choose a Number between 1 and 30 and don’t use the word Gloom as a Monster name. Do not put a backtick at the beginning and the end!`
+					} else {
+						prompt = `Create a detailed and unique Homebrew DnD monster you choose the monster name, type, are different 
+					each time and for the CR choose a Number between 1 and 30 and don’t use the word Gloom as a Monster name. Do not put a backtick at the beginning and the end!`
+					}
+				} else {
+					name := r.FormValue("name")
+					cr := r.FormValue("cr")
+					monsterType := r.FormValue("type")
+					monsterInfo := r.FormValue("monsterinfo")
+					prompt = `Create a detailed DnD monster sheet for the plutonium importer tool from 5etools for the import on Foundry Vtt. Include all necessary details such as name, type, abilities, 
+				and stats. Respond with the monster sheet in a clear, readable format without backticks at the beginning and the end.
+				`
+					if name != "" {
+						prompt += "\nName: " + name
+					}
+					if cr != "" {
+						prompt += "\nCR: " + cr
+					}
+					if monsterType != "" {
+						prompt += "\nType: " + monsterType
+					}
+					if monsterInfo != "" {
+						prompt += "\nDetails: " + monsterInfo
+					}
 				}
 			}
 
